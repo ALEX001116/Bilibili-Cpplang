8859c23c7cc0e0c451ee5c6ff9c0f4d6ce977f8a
nichijoufzl <137053460+ALEX001116@users.noreply.github.com>
2025-03-23 15:21:54 +0800

    Update 7.initialize_list.cpp

--- 2.第2章：封装/第二章代码/7.initialize_list.cpp
+++ 2.第2章：封装/第二章代码/7.initialize_list.cpp
@@ -1,9 +1,16 @@
 /*************************************************************************
-	> File Name: 7.initialize_list.cpp
-	> Author: hug
-	> Mail: hug@haizeix.com
-	> Created Time: 六  7/ 6 14:59:56 2024
- ************************************************************************/
+    > File Name: 7.initialize_list.cpp
+    > Author: hug
+    > Mail: hug@haizeix.com
+    > Created Time: 六  7/ 6 14:59:56 2024
+ /************************************************************************/
+ 
+ //初始化列表的注意事项：初始化列表中变量的执行顺序，只和这些变量在类中的定义顺序有关
+ 
+ //为什么要使用初始化列表？*看下面test3
+ 
+ /*为什么一定要使用初始化列表：效率问题。省去了赋值过程，调用的直接就是相关字段的构造函数*/
+ 
 
 #include <iostream>
 using namespace std;
@@ -12,8 +19,8 @@ namespace test1 {
 
 class A {
 public :
-    A() : A(1, 1) {}
-    A(int x, int y) : x(x), y(y) {}
+    A() : A(1, 1) {}//委托构造，默认赋值
+    A(int x, int y) : x(x), y(y) {}//初始化列表
     void output() {
         cout << "(" << x << ", " << y << ")" << endl;
     }
@@ -26,10 +33,14 @@ int main() {
     A a(3, 4), b(101, 202), c;
     a.output();
     b.output();
-    c.output();
+    c.output();//（1，1）
     return 0;
 }
 } // test1
+
+
+/********************************************/
+
 
 namespace test2 {
 
@@ -54,7 +65,7 @@ public :
     }
 
 private:
-    B x, y;
+    B x, y;//调用B类构造了x和y对象，类的组合
 };
 
 int main() {
@@ -65,12 +76,15 @@ int main() {
 
 } // test2
 
+/********************************************/
+
+ /*为什么要使用初始化列表？*/
 namespace test3 {
 
 class B {
 public :
     B() {
-        cout << this << " default constructor" << endl;
+        cout << this << " default constructor" << endl;//①
     }
     B(int x) : x(x) {
         cout << this << " : Class B constructor" << endl;
@@ -82,12 +96,19 @@ class A {
 public :
     A() : A(1, 1) {}
     A(int x, int y) : x(x), y(y) {}
+    //初始化列表不只是对x和y进行了赋值，更重要的是完成了他俩的构造过程。系统直接调用了x和y相关的有参构造。即上面的Class B constructor
+    
+    //如果不用初始化列表，这里改用为this->x.x=z和this->y.x=z，会怎么样呢？编译不通过。为什么？
+    //1.前面已经出现了有参构造，默认构造就会被删除
+    //2.对象的生命周期，一定要先经历构造过程。但是在{}这个大括号里面，已经使用了this->x这个对象，但又没有显示地去调用有参构造，对于系统而言就一定会去调用有参构造的呀。
+    //解决方法：①给上面的B类添加一个默认构造，看上面①
+    //解决方法二（重点）：初始化列表
     void output() {
         cout << "(" << x.x << ", " << y.x << ")" << endl;
     }
 
 private:
-    B x, y;
+    B x, y;//通过B类创建了对象
 };
 
 int main() {
